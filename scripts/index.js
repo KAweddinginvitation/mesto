ef36569a0c1b8bc3391cceb9ed5c9a040d54230a
Muesliestein <sv.kovalskiy@gmail.com>
2023-05-08 18:21:50 +0300

    dev

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.popup');
-const buttonPopupOpen = document.querySelector('.profile__info_edit-opened')
-const buttonPopupClose = document.querySelector('.popup__profile_close');
-const buttonPopupSave = document.querySelector('.popup__profile_save');
+const buttonPopupOpen = document.querySelector('.profile__info-opened')
+const buttonPopupClose = document.querySelector('.popup__profile-close');
+const buttonPopupSave = document.querySelector('.popup__profile-save');
 
 buttonPopupOpen.addEventListener('click', () => {
     popup.classList.add('popup__opened');
@@ -15,10 +15,10 @@ buttonPopupSave.addEventListener('click', () => {
     popup.classList.remove('popup__opened');
 });
 
-document.querySelector('.popup__profile_save').addEventListener('click', userClick);
+document.querySelector('.popup__profile-save').addEventListener('click', userClick);
 function userClick() {
-    let userName = document.querySelector('.popup__profile_username').value;
-    let userJob = document.querySelector('.popup__profile_job').value;
-    document.querySelector('.profile__info_title').textContent = userName;
-    document.querySelector('.profile__info_subtitle').textContent = userJob;
+    let userName = document.querySelector('.popup__profile-username').value;
+    let userJob = document.querySelector('.popup__profile-job').value;
+    document.querySelector('.profile__info-title').textContent = userName;
+    document.querySelector('.profile__info-subtitle').textContent = userJob;
 };
