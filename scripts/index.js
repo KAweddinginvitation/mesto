9f10780a4555de86f7c1bcb63df3629854b4b048
Muesliestein <sv.kovalskiy@gmail.com>
2023-05-11 00:28:54 +0300

    11.05

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,59 @@
 const popup = document.querySelector('.popup');
-const buttonPopupOpen = document.querySelector('.profile__info-edit')
-const buttonPopupClose = document.querySelector('.popup__profile-close');
-const buttonPopupSave = document.querySelector('.popup__profile-save');
-
+const buttonPopupOpen = document.querySelector('.profile__edit')
+const buttonPopupClose = document.querySelector('.popup__close');
+const buttonPopupSave = document.querySelector('.popup__save');
+//
+let nameInput = document.querySelector('name');
+let userName = document.querySelector('.profile__title');
+//
 buttonPopupOpen.addEventListener('click', () => {
-    popup.classList.add('popup_opened');
+    popup.classList.add('popup__opened');
+    nameInput.value = userName.textContent;
 });
 
 buttonPopupClose.addEventListener('click', () => {
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup__opened');
 });
 
 buttonPopupSave.addEventListener('click', () => {
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup__opened');
 });
 
-document.querySelector('.popup__profile-save').addEventListener('click', userClick);
+
+document.querySelector('.popup__save').addEventListener('submit', userClick);
 function userClick() {
-    let userName = document.querySelector('.popup__profile-username').value;
-    let userJob = document.querySelector('.popup__profile-job').value;
-    document.querySelector('.profile__info-title').textContent = userName;
-    document.querySelector('.profile__info-subtitle').textContent = userJob;
+
+    document.querySelector('.profile__title').textContent = userName;
+    document.querySelector('.profile__subtitle').textContent = userJob;
+    
 };
+
+//
+//
+//
+
+/* 
+// Находим форму в DOM
+let formElement = document.querySelector('form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('name')
+let descriptionInput = document.querySelector('description')
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+    nameInput.value = userName.textContent;
+    descriptionInput.value = userDescription.textContent;
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit); */
